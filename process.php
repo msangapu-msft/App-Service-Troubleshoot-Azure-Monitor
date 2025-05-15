e0d940001ef5be3a30fd48d2b31daac46388f40a
msangapu-msft <msangapu-msft@users.noreply.github.com>
2025-05-15 13:50:22 -0500

    working updates

--- process.php
+++ process.php
@@ -40,30 +40,13 @@ try {
     // Initialize image array to avoid undefined variable warning
     $imgArray = [];
 
-    // Load JPEGs into an array (in memory)
-    for ($x = 0; $x < $maxImages; $x++) {
-        // Defensive check for image name existence
-        if (isset($imgNames[$x]) && !empty($imgNames[$x])) {
-            $imgPath = "./images/" . $imgNames[$x];
-            if (file_exists($imgPath)) {
-                $imgArray[$x] = imagecreatefromjpeg($imgPath);
-            } else {
-                error_log("Image file not found: $imgPath");
-            }
-        } else {
-            error_log("Image name missing for index $x");
-        }
-    }
 
     // Loop through array and convert each JPEG to PNG
-    if (!empty($imgArray)) {
-        for ($x = 0; $x < $maxImages; $x++) {
-            if (isset($imgArray[$x], $imgNames[$x])) {
-                $filename = './images/converted_' . substr($imgNames[$x], 0, -4) . '.png';
-                imagepng($imgArray[$x], $filename);
-            }
-        }
+    for ($x=0; $x<$maxImages; $x++){
+        $filename = './images/converted_' . substr($imgNames[$x],0,-4) . '.png';
+        imagepng(imagecreatefromjpeg("./images/" . $imgNames[$x]), $filename);
     }
+
 
 } catch (Exception $e) {
     // Catch application-level exceptions
