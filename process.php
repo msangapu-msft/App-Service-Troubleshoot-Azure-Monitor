db4a25b6c3197a963daad7e98edb00a74e6b144b
Mangesh Sangapu <msangapu@microsoft.com>
2025-05-15 11:29:50 -0500

    Update process.php

--- process.php
+++ process.php
@@ -3,8 +3,7 @@
 // increase execution time
 set_time_limit(0);
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('memory_limit', '32M');
 error_reporting(E_ALL);
 ini_set('log_errors', 1);
 ini_set('error_log', '/home/LogFiles/php_errors.log');
